8034ac5cd95fbae38bdffa7f5c147ff31e31d293
ebosseck <ebosseck@lunznet.de>
2021-05-09 21:47:00 +0200

    Debug Plugin

---
+++ virtualstudio/plugins/debugaction/debugactionlauncher.py
@@ -0,0 +1,43 @@
+import sys
+
+from config import PLUGIN_DIRECTORY
+from virtualstudio.common.action_manager.actionmanager import registerCategoryIcon
+from virtualstudio.common.structs.action.action_launcher import ActionLauncher
+from virtualstudio.common.tools.icontools import readPNGIcon
+
+class DebugActionLauncher(ActionLauncher):
+
+    def __init__(self):
+        super(DebugActionLauncher, self).__init__()
+        registerCategoryIcon(["Debug"], PLUGIN_DIRECTORY + "/assets/debug/icons/debug.png")
+
+    #region Metadata
+
+    def getName(self):
+        return "Debug Action"
+
+    def getIcon(self):
+        return readPNGIcon(PLUGIN_DIRECTORY + "/assets/debug/icons/debug.png")
+
+    def getCategory(self):
+        return ["Debug"]
+
+    def getAuthor(self):
+        return "eric"
+
+    def getVersion(self):
+        return (0,0,1)
+
+    def getID(self):
+        return "{}.{}.{}-{}".format(self.getAuthor(), self.getCategory(), self.getName(), self.getVersion())
+
+    def allowedControls(self):
+        return []
+
+    def getActionUI(self, control):
+        return ""
+
+    def getActionForControl(self, control):
+        pass
+
+    #endregion
